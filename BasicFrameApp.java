11aad1d789261ab81ae7d51bb6a05e18eb3bb4ed
melsha123 <melshatebeila22@gmail.com>
2025-07-30 00:50:33 -0700

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author MELSHA
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //declare variables
+      MyFirstFrame mff;
+      
+      //instantiate the frame
+      mff = new MyFirstFrame();
+    }
+    
+}
