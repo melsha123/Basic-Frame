1ec974b3d9f62049f4dec153ea9fb53ecfa00a33
melsha123 <melshatebeila22@gmail.com>
2025-07-30 22:05:41 -0700

    Add files via upload

---
+++ BasicFrameApp2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp2;
+
+import za.ac.tut.ui.MySecondFrame;
+
+/**
+ *
+ * @author MELSHA
+ */
+public class BasicFrameApp2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MySecondFrame msf;
+        
+        msf = new MySecondFrame();
+    }
+    
+}
